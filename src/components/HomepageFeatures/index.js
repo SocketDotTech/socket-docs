f062b478d1548b6e46f54e1a182f7e1de6d8f804
Shrey Keny <46216791+shreykeny@users.noreply.github.com>
2023-04-30 00:22:33 +0530

    misc

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -22,7 +22,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Earn',
+    title: 'Surge',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
