8d4a894ae3ecdc2b45b45aee05f752c86598d2c4
Dimitri <69167058+dimsome@users.noreply.github.com>
2024-04-16 16:08:12 +0400

    update favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
